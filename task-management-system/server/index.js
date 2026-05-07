d9affb8a1ca29ba85d556d6ced0922120aafd72a
pushkarpawar01 <pawarpushkarprakash123@gmail.com>
2026-05-08 00:52:31 +0530

    third commit

--- task-management-system/server/index.js
+++ task-management-system/server/index.js
@@ -141,7 +141,7 @@ app.delete('/api/tasks/:id', auth, async (req, res) => {
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('(.*)', (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 }
